feb2176470c7cbd38ac0a1155b47590f546d14a1
Matt Raffel <mattr@kiva.org>
2019-08-01 12:17:37 -0700

    updating the template

--- rust-env/keep/cargo_template/src/main.rs
+++ rust-env/keep/cargo_template/src/main.rs
@@ -1,6 +1,14 @@
-#[macro_use]
-extern crate log;
+#![allow(unused_imports)]
+
+#[macro_use] extern crate log;
+#[macro_use] extern crate serde_json;
+#[macro_use] extern crate serde_derive;
+
+
 extern crate env_logger;
+extern crate futures;
+extern crate indyrs as indy;
+extern crate rand;
 
 fn main() {
     env_logger::init();
